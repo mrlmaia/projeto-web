6cb34bc0db9c0d0917187171b7508c7c76b334a1
Murilo Maia <murilomaia.bb@gmail.com>
2020-12-02 03:20:46 -0300

    lint

--- bancoDadosBiblioteca.php
+++ bancoDadosBiblioteca.php
@@ -6,8 +6,8 @@ function criarConexao()
   $conexao = null;
 
   try {
-    //$conexao = new PDO('mysql:host=localhost; dbname=id15244215_mydb', 'id15244215_admin', 'Neodimio25.05');
     $conexao = new PDO('mysql:host=127.0.0.1; dbname=ProjetoWeb', 'root', 'root');
+    // $conexao = new PDO('mysql:host=localhost; dbname=id15566093_projetoweb', 'id15566093_root', 'Root@Password-1');
     $conexao->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $erro) {
@@ -24,7 +24,7 @@ function fecharConexao($conexao)
 
 function criarArquivo($erro)
 {
-  echo "<script>alert('Foi gerada uma exceção.');</script>";
+  echo "<script>alert('Ocorreu um erro durante a execução do sistema');</script>";
   date_default_timezone_set('America/Sao_Paulo');
   $dataErro = date('d/m/Y H:i:s');
   $arquivo = fopen('arquivoErro.txt', 'a+');
--- historicoBiblioteca.php
+++ historicoBiblioteca.php
@@ -1,98 +1,102 @@
 <?php
 
-    require_once('bancoDadosBiblioteca.php');
+require_once('bancoDadosBiblioteca.php');
 
-    function salvarHistorico($idHistorico, $data, $estado, $idConta, $idMorador){
-        try{	
-            $situacao = FALSE;    
-            $conexao = criarConexao();
-            if($estado == 1){  
-                $novoEstado = 2;    
-                $sql = "INSERT INTO tbHistorico(data, estado, idConta, idMorador) VALUES(:data, :estado, :idConta, :idMorador);";
-                $resultado = $conexao->prepare($sql);
-                $resultado->bindValue(':data', $data); 
-                $resultado->bindValue(':estado', $novoEstado); 
-                $resultado->bindValue(':idConta', $idConta); 
-                $resultado->bindValue(':idMorador', $idMorador); 
-            }else{
-                $novoEstado = 1;
-                $sql = "INSERT INTO tbHistorico(data, estado, idConta, idMorador) VALUES(:data, :estado, :idConta, :idMorador);";
-                $resultado = $conexao->prepare($sql);
-                $resultado->bindValue(':data', $data); 
-                $resultado->bindValue(':estado', $novoEstado); 
-                $resultado->bindValue(':idConta', $idConta); 
-                $resultado->bindValue(':idMorador', $idMorador);    
-            }
-            $resultado->execute(); 
-            fecharConexao($conexao);
-            if($resultado->rowCount() > 0){
-                $situacao = TRUE;
-            }        
-        }catch (PDOException $erro){
-            criarArquivo($erro);
-        }	        
-        return $situacao;
+function salvarHistorico($idHistorico, $data, $estado, $idConta, $idMorador)
+{
+  try {
+    $situacao = FALSE;
+    $conexao = criarConexao();
+    if ($estado == 1) {
+      $novoEstado = 2;
+      $sql = "INSERT INTO tbHistorico(data, estado, idConta, idMorador) VALUES(:data, :estado, :idConta, :idMorador);";
+      $resultado = $conexao->prepare($sql);
+      $resultado->bindValue(':data', $data);
+      $resultado->bindValue(':estado', $novoEstado);
+      $resultado->bindValue(':idConta', $idConta);
+      $resultado->bindValue(':idMorador', $idMorador);
+    } else {
+      $novoEstado = 1;
+      $sql = "INSERT INTO tbHistorico(data, estado, idConta, idMorador) VALUES(:data, :estado, :idConta, :idMorador);";
+      $resultado = $conexao->prepare($sql);
+      $resultado->bindValue(':data', $data);
+      $resultado->bindValue(':estado', $novoEstado);
+      $resultado->bindValue(':idConta', $idConta);
+      $resultado->bindValue(':idMorador', $idMorador);
     }
-
-    function excluirHistorico($idHistorico){
-        try{	
-            $situacao = FALSE;  
-            $sql = "DELETE FROM tbHistorico WHERE idHistorico = {$idHistorico};";
-            $conexao = criarConexao();
-            $resultado = $conexao->prepare($sql);	
-            $resultado->bindValue(':idHistorico', $idHistorico); 		
-            $resultado->execute(); 
-            fecharConexao($conexao);
-            if($resultado->rowCount() > 0){
-                $situacao = TRUE;
-            } 	
-        }catch (PDOException $erro){
-            criarArquivo($erro);
-        }	        	
-        return $situacao;
+    $resultado->execute();
+    fecharConexao($conexao);
+    if ($resultado->rowCount() > 0) {
+      $situacao = TRUE;
     }
+  } catch (PDOException $erro) {
+    criarArquivo($erro);
+  }
+  return $situacao;
+}
 
-    function listarHistorico(){
-        try{	        
-            $sql = "SELECT * FROM tbHistorico;";
-            $conexao = criarConexao();
-            $resultado = $conexao->prepare($sql);			
-            $resultado->execute();         
-            $registros = $resultado->fetchAll();
-            fecharConexao($conexao);
-        }catch (PDOException $erro){
-            criarArquivo($erro);
-        }	         
-        return $registros;
-    }  
+function excluirHistorico($idHistorico)
+{
+  try {
+    $situacao = FALSE;
+    $sql = "DELETE FROM tbHistorico WHERE idHistorico = {$idHistorico};";
+    $conexao = criarConexao();
+    $resultado = $conexao->prepare($sql);
+    $resultado->bindValue(':idHistorico', $idHistorico);
+    $resultado->execute();
+    fecharConexao($conexao);
+    if ($resultado->rowCount() > 0) {
+      $situacao = TRUE;
+    }
+  } catch (PDOException $erro) {
+    criarArquivo($erro);
+  }
+  return $situacao;
+}
 
-    function buscarHistorico($idHistorico){
-        try{          
-            $sql = "SELECT * FROM tbHistorico WHERE idHistorico = {$idHistorico};";
-            $conexao = criarConexao();
-            $resultado = $conexao->prepare($sql);	
-            $resultado->bindValue(':idHistorico', $idHistorico); 		
-            $resultado->execute(); 
-            $registro = $resultado->fetch();
-            fecharConexao($conexao);
-        }catch (PDOException $erro){
-            criarArquivo($erro);
-        }	         
-        return $registro;        
-    } 
+function listarHistorico()
+{
+  try {
+    $sql = "SELECT * FROM tbHistorico;";
+    $conexao = criarConexao();
+    $resultado = $conexao->prepare($sql);
+    $resultado->execute();
+    $registros = $resultado->fetchAll();
+    fecharConexao($conexao);
+  } catch (PDOException $erro) {
+    criarArquivo($erro);
+  }
+  return $registros;
+}
 
-    function listarHistoricoPorConta($idConta){
-        try{	        
-            $sql = "SELECT * FROM tbHistorico WHERE idConta = :idConta;";
-            $conexao = criarConexao();
-            $resultado = $conexao->prepare($sql);	
-            $resultado->bindValue(':idConta', $idConta); 			
-            $resultado->execute();         
-            $registros = $resultado->fetchAll();
-            fecharConexao($conexao);
-        }catch (PDOException $erro){
-            criarArquivo($erro);
-        }	         
-        return $registros;
-    }      
-?>
+function buscarHistorico($idHistorico)
+{
+  try {
+    $sql = "SELECT * FROM tbHistorico WHERE idHistorico = {$idHistorico};";
+    $conexao = criarConexao();
+    $resultado = $conexao->prepare($sql);
+    $resultado->bindValue(':idHistorico', $idHistorico);
+    $resultado->execute();
+    $registro = $resultado->fetch();
+    fecharConexao($conexao);
+  } catch (PDOException $erro) {
+    criarArquivo($erro);
+  }
+  return $registro;
+}
+
+function listarHistoricoPorConta($idConta)
+{
+  try {
+    $sql = "SELECT * FROM tbHistorico WHERE idConta = :idConta;";
+    $conexao = criarConexao();
+    $resultado = $conexao->prepare($sql);
+    $resultado->bindValue(':idConta', $idConta);
+    $resultado->execute();
+    $registros = $resultado->fetchAll();
+    fecharConexao($conexao);
+  } catch (PDOException $erro) {
+    criarArquivo($erro);
+  }
+  return $registros;
+}
